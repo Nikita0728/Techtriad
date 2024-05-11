9cc6236256904c7c0105894ae24b50f6b0d7bb5f
Nikita0728 <kunwornikita123@gmail.com>
2024-05-11 13:11:07 +0545

    okay

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
-const Navbar = () => {
+const Navbars = () => {
   return (
+    <>
     <div>
-      heyyyy ladyyy
+    <Navbar bg="dark" data-bs-theme="dark" fixed='top'>
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    
     </div>
+    </>
   )
 }
 
-export default Navbar
+export default Navbars
